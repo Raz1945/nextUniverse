1c253c99cb50ebb3e7df6eb0fe191be04a38c75d
raz1945 <g.sebas.sanchez@gmail.com>
2024-01-09 23:37:20 -0300

    se agrega funcion para cancelar el update

--- next-universe/src/components/Installations/Installation.jsx
+++ next-universe/src/components/Installations/Installation.jsx
@@ -1,29 +1,48 @@
-export const Installation = (
-  { plantType, metalCost, crystalCost, deuteriumCost, currentLevel, picture, onClickUpdate, onClickCancel, countdown }
-) => {
+export const Installation = ({
+  plantType,
+  metalCost,
+  crystalCost,
+  deuteriumCost,
+  currentLevel,
+  picture,
+  onClickUpdate,
+  onClickCancel,
+  onClickDestroy,
+  countdown,
+}) => {
   return (
     <div>
-      <p>Planta de {plantType} - Nivel actual: {currentLevel} --&gt; {currentLevel + 1}</p>
+      <p>
+        Planta de {plantType} - Nivel actual: {currentLevel} --&gt;{' '}
+        {currentLevel + 1}
+      </p>
+      
       <p>Metal: {metalCost}</p>
       <p>Crystal: {crystalCost}</p>
       {deuteriumCost > 0 && <p>Deuterio: {deuteriumCost}</p>}
+
       <picture>
-        <img src={picture} alt="Picture of installation" />
+        <img src={picture} alt='Picture of installation' />
       </picture>
 
       <h3>{countdown}</h3>
+
       <ul>
         <li>
-          <button type="button" onClick={onClickUpdate}>
+          <button type='button' onClick={onClickUpdate}>
             Mejorar
           </button>
-          <button type="button" onClick={onClickCancel}>
+        </li>
+        <li>
+          <button type='button' onClick={onClickCancel}>
             Cancelar
           </button>
-          {/* <button type="button" onClick={() => handleOnClickUpdate('metalMine')}>Mejorar</button> */}
         </li>
-        <li>Cancelar</li>
-        <li>Demoler</li>
+        <li>
+          <button type='button' onClick={onClickDestroy}>
+            Demoler
+          </button>
+        </li>
       </ul>
     </div>
   );
--- next-universe/src/pages/Installations.jsx
+++ next-universe/src/pages/Installations.jsx
@@ -15,130 +15,142 @@ export const Installations = () => {
     nanobotFactory: 0,
     // otras instalaciones
   });
-  // 
-  const [countdown, setCountdown] = useState("");
 
-  
+  const [countdown, setCountdown] = useState("");
+  const [updateInProgress, setUpdateInProgress] = useState(false);
+  const [countdownInterval, setCountdownInterval] = useState(null);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        await fetchResource();
-      } catch (error) {
-        console.error("Error al obtener los valores de recursos:", error);
-      }
-    };
-
     fetchData();
   }, []);
 
   useEffect(() => {
-    const fetchInstallationLevels = async () => {
-      try {
-        const { resource: levels } = await getInstallationCurrentLevel();
-        setResourceCurrentLevels((prevLevels) => ({
-          ...prevLevels,
-          metalMine: levels?.metalMine || 0,
-          crystalMine: levels?.crystalMine || 0,
-          robotFactory: levels?.robotFactory || 0,
-          nanobotFactory: levels?.nanobotFactory || 0,
-          // Otras instalaciones 
-        }));
-      } catch (error) {
-        console.error("Error al obtener los niveles de instalación", error);
-      }
-    };
-
     fetchInstallationLevels();
   }, [resourceCurrentLevels]);
 
+  const fetchData = async () => {
+    try {
+      await fetchResource();
+    } catch (error) {
+      console.error("Error al obtener los valores de recursos:", error);
+    }
+  };
+
+  const fetchInstallationLevels = async () => {
+    try {
+      const { resource: levels } = await getInstallationCurrentLevel();
+      setResourceCurrentLevels((prevLevels) => ({
+        ...prevLevels,
+        metalMine: levels?.metalMine || 0,
+        crystalMine: levels?.crystalMine || 0,
+        robotFactory: levels?.robotFactory || 0,
+        nanobotFactory: levels?.nanobotFactory || 0,
+        // Otras instalaciones 
+      }));
+    } catch (error) {
+      console.error("Error al obtener los niveles de instalación", error);
+    }
+  };
+
   const handleOnClickUpdate = async (plantType) => {
+    if (updateInProgress) {
+      console.log("Ya hay una actualización en curso");
+      return;
+    }
+
+    setUpdateInProgress(true);
+
     const plantInfo = { plantType };
     const nextLevel = resourceCurrentLevels[plantType] + 1;
-  
+
     try {
       const costResourceResponse = await calculateSpecificInstallationCost(plantType, nextLevel);
       const { metalCost, crystalCost, deuteriumCost } = costResourceResponse;
-  
       const { ecoUniverse, metal, crystal, deuterium } = await fetchResource();
-  
-      // Validación adicional para asegurarse de que los costos de recursos no sean negativos
+
       if (metalCost < 0 || crystalCost < 0 || deuteriumCost < 0) {
         console.error('Error: Los costos de recursos no pueden ser negativos.');
         return;
       }
-  
-      // Si los recursos son suficientes para construir la siguiente planta, actualiza la planta y los recursos
+
       if (metal >= metalCost && crystal >= crystalCost && deuterium >= deuteriumCost) {
         const { robotFactory, nanobotFactory } = resourceCurrentLevels;
-  
-        // Calcula el tiempo de construcción y la hora de finalización de la planta actualizada
         const { timeToBuild, endTime } = calculateTimeBuild(ecoUniverse, metalCost, crystalCost, robotFactory, nanobotFactory);
-  
+
         console.log(`Tiempo de construcción estimado para ${plantType}:`);
         console.log(`Horas: ${timeToBuild.hours}, Minutos: ${timeToBuild.minutes}, Segundos: ${timeToBuild.seconds}`);
         console.log(`Hora estimada de finalización: ${endTime}`);
-  
-        // Inicia el contador de tiempo de construcción
-        const countdownInterval = setInterval(() => {
-          if (timeToBuild.seconds > 0 || timeToBuild.minutes > 0 || timeToBuild.hours > 0) {
-            timeToBuild.seconds -= 1;
-  
-            if (timeToBuild.seconds === 0 && (timeToBuild.minutes > 0 || timeToBuild.hours > 0)) {
-              timeToBuild.minutes -= 1;
-              timeToBuild.seconds = 59;
+
+        setCountdownInterval(
+          setInterval(() => {
+            if (timeToBuild.seconds > 0 || timeToBuild.minutes > 0 || timeToBuild.hours > 0) {
+              timeToBuild.seconds -= 1;
+
+              if (timeToBuild.seconds === 0 && (timeToBuild.minutes > 0 || timeToBuild.hours > 0)) {
+                timeToBuild.minutes -= 1;
+                timeToBuild.seconds = 59;
+              }
+
+              if (timeToBuild.minutes === 0 && timeToBuild.hours > 0) {
+                timeToBuild.hours -= 1;
+                timeToBuild.minutes = 59;
+                timeToBuild.seconds = 59;
+              }
+
+              console.log(`Tiempo restante: ${timeToBuild.hours} horas, ${timeToBuild.minutes} minutos, ${timeToBuild.seconds} segundos`);
+              setCountdown(`${timeToBuild.hours}:${timeToBuild.minutes}:${timeToBuild.seconds}`);
+            } else {
+              clearInterval(countdownInterval);
+              updateResourcesAndLevel(plantType, plantInfo, metal, crystal, deuterium, metalCost, crystalCost, deuteriumCost);
             }
-  
-            if (timeToBuild.minutes === 0 && timeToBuild.hours > 0) {
-              timeToBuild.hours -= 1;
-              timeToBuild.minutes = 59;
-              timeToBuild.seconds = 59;
-            }
-  
-            console.log(`Tiempo restante: ${timeToBuild.hours} horas, ${timeToBuild.minutes} minutos, ${timeToBuild.seconds} segundos`);
-            
-            // Actualiza el estado del temporizador
-            setCountdown(`${timeToBuild.hours}:${timeToBuild.minutes}:${timeToBuild.seconds}`);
-          } else {
-            clearInterval(countdownInterval);
-  
-            // Realizamos la actualización
-            (async () => {
-              const updatedMetalProductionValue = metal - metalCost;
-              const updatedCrystalProductionValue = crystal - crystalCost;
-              const updatedDeuteriumProductionValue = deuterium - deuteriumCost;
-  
-              const updatedResources = {
-                metalProduction: updatedMetalProductionValue,
-                crystalProduction: updatedCrystalProductionValue,
-                deuteriumProduction: updatedDeuteriumProductionValue,
-              };
-  
-              await Promise.all([
-                updateResourceValue(updatedResources),
-                updateAllResources(updatedResources),
-                updateInstallationCurrentLevel(plantInfo),
-              ]);
-  
-              console.log(`Planta de ${plantType} actualizada`);
-  
-              // Restablece el estado del temporizador después de la finalización
-              setCountdown("");
-            })();
-          }
-        }, 1000);
-  
+          }, 1000)
+        );
       } else {
         console.log(`Recursos insuficientes para actualizar la planta de ${plantType}`);
+        setUpdateInProgress(false);
       }
     } catch (error) {
       console.error(`Error updating ${plantType} plant:`, error);
       alert('Failed to update plant. Please try again.');
+      setUpdateInProgress(false);
     }
   };
 
-  const handleOnClickCancele = () => {
+
+  const handleOnClickCancel = () => {
     console.log('Cancelar proceso de actualización');
+    clearInterval(countdownInterval);
+    setCountdown("");
+    setUpdateInProgress(false);
+  };
+
+
+  // vuelve un nivel para atras
+  const handleOnClickDestroy = () => {
+    console.log('Destruir la instalación');
+  };
+
+
+  const updateResourcesAndLevel = async (plantType, plantInfo, metal, crystal, deuterium, metalCost, crystalCost, deuteriumCost) => {
+    const updatedMetalProductionValue = metal - metalCost;
+    const updatedCrystalProductionValue = crystal - crystalCost;
+    const updatedDeuteriumProductionValue = deuterium - deuteriumCost;
+
+    const updatedResources = {
+      metalProduction: updatedMetalProductionValue,
+      crystalProduction: updatedCrystalProductionValue,
+      deuteriumProduction: updatedDeuteriumProductionValue,
+    };
+
+    await Promise.all([
+      updateResourceValue(updatedResources),
+      updateAllResources(updatedResources),
+      updateInstallationCurrentLevel(plantInfo),
+    ]);
+
+    console.log(`Planta de ${plantType} actualizada`);
+    setCountdown("");
+    setUpdateInProgress(false);
   };
 
   const updateAllResources = async (updatedResources) => {
@@ -169,10 +181,10 @@ export const Installations = () => {
         currentLevel={resourceCurrentLevels.metalMine}
         metalCost={Math.round(calculateSpecificInstallationCost('metalMine', resourceCurrentLevels.metalMine + 1).metalCost)}
         crystalCost={Math.round(calculateSpecificInstallationCost('metalMine', resourceCurrentLevels.metalMine + 1).crystalCost)}
-
         countdown={countdown}
         onClickUpdate={() => handleOnClickUpdate('metalMine')}
-        onClickCancele={() => handleOnClickCancele('metalMine')}
+        onClickCancel={() => handleOnClickCancel()}
+        onClickDestroy={() => handleOnClickDestroy('metalMine')}
       />
       <br />
       {/* ... otras instalaciones */}
